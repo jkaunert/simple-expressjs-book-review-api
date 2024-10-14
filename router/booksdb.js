3829b982b528ed5efd8f98f392a4cebdcf814a82
joshua kaunert <4586402+jkaunert@users.noreply.github.com>
2024-10-14 12:46:26 -0500

    adds public no-auth endpoints

--- router/booksdb.js
+++ router/booksdb.js
@@ -1,14 +1,14 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+      1: { "isbn": "0385474547", "author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
+      2: { "isbn": "1445508583", "author": "Hans Christian Andersen","title": "Fairy tales", "reviews": { "user1": "Great book!" } },
+      3: { "isbn": "0451208633", "author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
+      4: { "isbn": "014044100X", "author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
+      5: { "isbn": "0060969598", "author": "Unknown","title": "The Book Of Job", "reviews": {} },
+      6: { "isbn": "1463794533", "author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
+      7: { "isbn": "0140447695", "author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
+      8: { "isbn": "1441341706", "author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
+      9: { "isbn": "9780199538751", "author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
+      10: { "isbn": "0802144470", "author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
 }
 
 module.exports=books;
--- router/general.js
+++ router/general.js
@@ -1,5 +1,6 @@
 const express = require('express');
 let books = require("./booksdb.js");
+const { console } = require('inspector');
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
@@ -12,32 +13,65 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
   return res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/isbn/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  if (isbn) {
+    for (let book of Object.values(books)) {
+      if (book.isbn === isbn) {
+        return res.send(book);
+      }
+    }
+    return res.status(404).json({ message: ` Book with ISBN {isbn} not found` });
+  }
  });
-  
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  if (author) {
+    const booksByAuthor = [];
+    for (let book of Object.values(books)) {
+      if (book.author == author) {
+       booksByAuthor.push(book);
+      }
+    }
+    return res.send(booksByAuthor);
+  } else {
+    return res.status(404).json({ message: `Author {author} not found` });
+  }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  if (title) {
+    const booksByTitle = [];
+    for (let book of Object.values(books)) {
+      if (book.title == title) {
+        booksByTitle.push(book);
+      }
+    }
+    return res.send(booksByTitle);
+  } else {
+    return res.status(404).json({ message: `Title {title} not found` });
+  }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  if (isbn) {
+    for (let book of Object.values(books)) {
+      if (book.isbn === isbn) {
+        return res.send(book.reviews);
+      }
+    }
+    return res.status(404).json({ message: ` Book with ISBN {isbn} not found` });
+  }
 });
 
 module.exports.general = public_users;
