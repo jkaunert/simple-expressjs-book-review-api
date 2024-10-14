9f68536895a4b2b88483456a6d4f968cf0396d62
joshua kaunert <4586402+jkaunert@users.noreply.github.com>
2024-10-14 16:31:44 -0500

    clean up

--- router/general.js
+++ router/general.js
@@ -21,11 +21,6 @@ public_users.post("/register", (req,res) => {
   return res.status(400).json({ message: "Unable to register user" });
 });
 
-// Get the book list available in the shop
-// public_users.get('/',function (req, res) {
-//   return res.send(JSON.stringify(books, null, 4));
-// });
-
 // get all books using async/await
 public_users.get('/', async (req, res) => {
   try {
@@ -58,18 +53,6 @@ public_users.get('/isbn/:isbn', async (req, res) => {
     return res.status(500).json({ message: "Internal server error: " + error.message });
   }
 });
-// Get book details based on ISBN
-// public_users.get('/isbn/:isbn', function (req, res) {
-//   const isbn = req.params.isbn;
-//   if (isbn) {
-//     for (let book of Object.values(books)) {
-//       if (book.isbn === isbn) {
-//         return res.send(book);
-//       }
-//     }
-//     return res.status(404).json({ message: ` Book with ISBN {isbn} not found` });
-//   }
-// });
 
 const getBooksByAuthor = async (author) => {
   const booksByAuthor = [];
@@ -93,21 +76,6 @@ public_users.get('/author/:author', async (req, res) => {
     return res.status(500).json({ message: "Internal server error: " + error.message });
   };
 });
-// Get book details based on author
-// public_users.get('/author/:author',function (req, res) {
-//   const author = req.params.author;
-//   if (author) {
-//     const booksByAuthor = [];
-//     for (let book of Object.values(books)) {
-//       if (book.author == author) {
-//        booksByAuthor.push(book);
-//       }
-//     }
-//     return res.send(booksByAuthor);
-//   } else {
-//     return res.status(404).json({ message: `Author ${author} not found` });
-//   }
-// });
 
 const getBooksByTitle = async (title) => {
   const booksByTitle = [];
@@ -131,21 +99,6 @@ public_users.get('/title/:title', async (req, res) => {
     return res.status(500).json({ message: "Internal server error: " + error.message });
   }
 });
-// Get all books based on title
-// public_users.get('/title/:title',function (req, res) {
-//   const title = req.params.title;
-//   if (title) {
-//     const booksByTitle = [];
-//     for (let book of Object.values(books)) {
-//       if (book.title == title) {
-//         booksByTitle.push(book);
-//       }
-//     }
-//     return res.send(booksByTitle);
-//   } else {
-//     return res.status(404).json({ message: `Title ${title} not found` });
-//   }
-// });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
